a9c0fb7bf068af77260c35e09da83e6316e97b18
Zeropage <Zeropage>
2016-01-08 14:46:43 +0900

    Add test login activity.

---
+++ app/src/main/java/org/zeropage/causcheduler/Activity/LoginActivity.java
@@ -0,0 +1,18 @@
+package org.zeropage.causcheduler.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.zeropage.causcheduler.R;
+
+/**
+ * Created by Lumin on 2016-01-08.
+ */
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
