47fa70545f0bc42b54ab3bf44fe092c6d9f65aa1
Zeropage <Zeropage>
2016-01-08 16:34:06 +0900

    Refactor login activity.

--- app/src/main/java/org/zeropage/causcheduler/Activity/LoginActivity.java
+++ app/src/main/java/org/zeropage/causcheduler/Activity/LoginActivity.java
@@ -22,13 +22,22 @@ import org.zeropage.causcheduler.Util.SharedConstant;
 /**
  * Created by Lumin on 2016-01-08.
  */
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements Response.Listener{
     private static final String LOG_TAG = "LoginProcess";
+    private final ProgressDialog dialog = new ProgressDialog(LoginActivity.this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // 로그인 작업을 위한 Dialog 설정.
+        String dialogHeader = RConverter.getString(getApplicationContext(), R.string.login_introduction_header);
+        String dialogMessage = RConverter.getString(getApplicationContext(), R.string.login_introduction_msg);
+        dialog.setTitle(dialogHeader);
+        dialog.setMessage(dialogMessage);
+        dialog.setIndeterminate(false);
+        dialog.setCancelable(false);
 
         final Button loginButton = (Button) findViewById(R.id.login_button);
         final EditText idText = (EditText) findViewById(R.id.portal_id);
@@ -40,59 +49,58 @@ public class LoginActivity extends AppCompatActivity {
         Log.e(LOG_TAG, "현재 저장되어 있는 학생 학번 : " + prefs.getString(RConverter.getString(getApplicationContext(), R.string.student_num_key), ""));
         Log.e(LOG_TAG, "현재 저장되어 있는 학생 학과 : " + prefs.getString(RConverter.getString(getApplicationContext(), R.string.student_dept_key), ""));
 
-        if (!checkLoginStatus()) {
-
-        } else {
-            switchActivity(MainActivity.class);
-            Toast.makeText(getApplicationContext(), "환영합니다.", Toast.LENGTH_LONG).show();
-        }
-
-
-
-        // 테스트 용.
+        // 버튼에 Listener 추가.
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 로그인 작업을 위한 Dialog 생성.
-                String dialogHeader = RConverter.getString(getApplicationContext(), R.string.login_introcution_header);
-                String dialogMessage = RConverter.getString(getApplicationContext(), R.string.login_introduction_msg);
-                final ProgressDialog dialog = ProgressDialog.show(LoginActivity.this, dialogHeader, dialogMessage, false, false);
-
-                PortalNetworkQueue.sendLoginRequest(LoginActivity.this, idText.getText().toString(), pwText.getText().toString(), new Response.Listener() {
-                    @Override
-                    public void onResponse(Object response) {
-                        String responseInfo = response.toString();
-                        String[] studentInfo  = responseInfo.split(",");
-
-                        // NG2가 Response로 나옴. 즉, 로그인이 실패함.
-                        if (studentInfo.length == 1) {
-                            Toast.makeText(getApplicationContext(), "잘못된 아이디.", Toast.LENGTH_LONG).show();
-                        } else {
-                            prefs.edit().putString(RConverter.getString(getApplicationContext(), R.string.student_name_key), studentInfo[0]).apply();
-                            prefs.edit().putString(RConverter.getString(getApplicationContext(), R.string.student_num_key), studentInfo[1]).apply();
-                            prefs.edit().putString(RConverter.getString(getApplicationContext(), R.string.student_dept_key), studentInfo[2]).apply();
-
-                            // For Logging.
-                            Log.e(LOG_TAG, "받아온 학생 이름 : " + studentInfo[0]);
-                            Log.e(LOG_TAG, "받아온 학생 학번 : " + studentInfo[1]);
-                            Log.e(LOG_TAG, "받아온 학생 학과 : " + studentInfo[2]);
-
-                            switchActivity(MainActivity.class);
-                        }
-
-                        // Dialog 해제.
-                        dialog.dismiss();
-                    }
-                });
+                PortalNetworkQueue.sendLoginRequest(LoginActivity.this, idText.getText().toString(), pwText.getText().toString(), LoginActivity.this);
             }
         });
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // 이미 로그인이 되어있는 상태인지 검사.
+        if (isAlreadyLoginStatus()) {
+            switchActivity(MainActivity.class);
+            Toast.makeText(getApplicationContext(), RConverter.getString(getApplicationContext(), R.string.welcome_msg), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onResponse(Object response) {
+        final String responseInfo = response.toString();
+        final String[] studentInfo  = responseInfo.split(",");
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        // NG2가 Response로 나옴. 즉, 로그인이 실패함.
+        if (studentInfo.length == 1) {
+            Toast.makeText(getApplicationContext(), RConverter.getString(getApplicationContext(), R.string.login_fail_msg), Toast.LENGTH_LONG).show();
+        } else {
+            // 학생 정보를 저장함.
+            prefs.edit().putString(RConverter.getString(getApplicationContext(), R.string.student_name_key), studentInfo[0]).apply();
+            prefs.edit().putString(RConverter.getString(getApplicationContext(), R.string.student_num_key), studentInfo[1]).apply();
+            prefs.edit().putString(RConverter.getString(getApplicationContext(), R.string.student_dept_key), studentInfo[2]).apply();
+
+            // For Logging.
+            Log.e(LOG_TAG, "받아온 학생 이름 : " + studentInfo[0]);
+            Log.e(LOG_TAG, "받아온 학생 학번 : " + studentInfo[1]);
+            Log.e(LOG_TAG, "받아온 학생 학과 : " + studentInfo[2]);
+
+            switchActivity(MainActivity.class);
+        }
+
+        // Dialog 해제.
+        dialog.dismiss();
     }
 
     /**
      * 로그인이 되어있는 상태인지 확인합니다.
      * @return 로그인이 되어있는 상태면 True, 되어있지 않으면 False를 반환합니다.
      */
-    private boolean checkLoginStatus() {
+    private boolean isAlreadyLoginStatus() {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         String savedStudentId = prefs.getString(RConverter.getString(getApplicationContext(), R.string.student_num_key), SharedConstant.EMPTY_STRING);
         String savedStudentName = prefs.getString(RConverter.getString(getApplicationContext(), R.string.student_name_key), SharedConstant.EMPTY_STRING);
@@ -106,7 +114,7 @@ public class LoginActivity extends AppCompatActivity {
      */
     private void switchActivity(Class<?> cls) {
         Intent switchIntent = new Intent(getApplicationContext(), cls);
-        this.finish();
+        finish();
         startActivity(switchIntent);
     }
 }
