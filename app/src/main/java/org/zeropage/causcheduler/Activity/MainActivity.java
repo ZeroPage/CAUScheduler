bfac9b5c236832bd962744e9fc83b23f5735ab41
Donghwan Kim <wraithtorch@gmail.com>
2016-01-09 05:26:13 +0900

    now drawer covers content view when it opened

--- app/src/main/java/org/zeropage/causcheduler/Activity/MainActivity.java
+++ app/src/main/java/org/zeropage/causcheduler/Activity/MainActivity.java
@@ -1,13 +1,16 @@
 package org.zeropage.causcheduler.Activity;
 
 import android.content.res.Configuration;
+import android.support.annotation.NonNull;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -20,6 +23,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private final String LOG_TAG = MainActivity.class.getSimpleName();
 
+    private Toolbar toolbar;
     private List<String> mDrawerItemList;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
@@ -31,11 +35,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Toolbar 초기화
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         // Drawer List 초기화
         String[] drawerItemArray = getResources().getStringArray(R.array.drawer_item_array);
         mDrawerItemList = Arrays.asList(drawerItemArray);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList = (ListView) findViewById(R.id.drawer);
 
         // Set the adapter for the list view
         mDrawerList.setAdapter(new ArrayAdapter<>(this,
@@ -45,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Log.v(LOG_TAG, (String)mDrawerList.getItemAtPosition(i));
+                mDrawerLayout.closeDrawer(mDrawerList);// 선택 후 드로어를 닫음
             }
         });
 
@@ -71,15 +80,20 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the drawer toggle as the DrawerListener
         mDrawerLayout.setDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     /* Called whenever we call invalidateOptionsMenu() */
+    // 이 메소드는 드로어가 열릴 경우, 그에 따라 자연스럽게 화면에서 특정 컴포넌트를 없애는 데 사용합니다.
+    // ex) 드로어가 열리면 툴바의 검색 버튼이 자동으로 사라져야 함.
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
         // If the nav drawer is open, hide action items related to the content view
+        // boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
         return super.onPrepareOptionsMenu(menu);
     }
 
+    // onPostCreate와 onConfigurationChanged는 툴바의 토글 상태를 동기화 하기 위해 필요함
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
@@ -93,4 +107,18 @@ public class MainActivity extends AppCompatActivity {
         mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
+    // 홈버튼 이벤트가 툴바 토글에서 끝나도록 오버라이드한 메소드
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
