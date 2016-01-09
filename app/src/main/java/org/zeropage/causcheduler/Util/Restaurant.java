85d55a118f292d6e512f232081fff2e6bb89d170
nErumin <nErumin90@gmail.com>
2016-01-09 16:35:37 +0900

    Add Restaurant List.

---
+++ app/src/main/java/org/zeropage/causcheduler/Util/Restaurant.java
@@ -0,0 +1,50 @@
+package org.zeropage.causcheduler.Util;
+
+/**
+ * Created by Lumin on 2016-01-09.
+ * 이용 가능한 식당의 목록을 가져옵니다.
+ */
+public enum Restaurant {
+    /**
+     * University Club 식당을 가리킵니다.
+     */
+    UniversityClub("11"),
+    /**
+     * 교직원 식당을 가리킵니다.
+     */
+    EmployeeRestourant("07"),
+    /**
+     * 신 기숙사 식당을 가리킵니다.
+     */
+    NewDormitory("12"),
+    /**
+     * 기존 기숙사 식당을 가리킵니다.
+     */
+    Dormitory("08"),
+    /**
+     * 법학관 식당을 가리킵니다.
+     */
+    RawBuilding("06"),
+    /**
+     * 참마루 식당을 가리킵니다.
+     */
+    CharmMaru("03"),
+    /**
+     * 슬기마루 식당을 가리킵니다.
+     */
+    SensibleMaru("02");
+
+    String code;
+
+    Restaurant(String code) {
+        this.code = code;
+    }
+
+    /**
+     * 해당 식당에 부여된 코드를 가져옵니다.
+     * @return 식당에 부여된 코드입니다.
+     */
+    public String getCode() {
+        return this.code;
+    }
+}
