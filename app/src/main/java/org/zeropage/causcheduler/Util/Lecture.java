a7af359b4672656a27099082115c85741ec1986f
nErumin <nErumin90@gmail.com>
2016-01-09 15:19:39 +0900

    Add basic format of Lecture class.

---
+++ app/src/main/java/org/zeropage/causcheduler/Util/Lecture.java
@@ -0,0 +1,12 @@
+package org.zeropage.causcheduler.Util;
+
+/**
+ * 한 강의에 대한 정보를 담고 있는 클래스입니다.
+ * Created by Lumin on 2016-01-09.
+ */
+public class Lecture {
+    private String lectureName;
+    private String professorName;
+    private String lectureDeptName;
+    private int sectionNum;
+}
