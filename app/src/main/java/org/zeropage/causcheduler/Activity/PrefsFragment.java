d5b8bc77fccf05db8ad52729873ce310773f1d3e
Donghwan Kim <wraithtorch@gmail.com>
2016-01-14 15:50:47 +0900

    make assignmentfragment, and need to add adapter to listView_assignment

--- app/src/main/java/org/zeropage/causcheduler/Activity/PrefsFragment.java
+++ app/src/main/java/org/zeropage/causcheduler/Activity/PrefsFragment.java
@@ -1,6 +1,5 @@
 package org.zeropage.causcheduler.Activity;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
@@ -8,13 +7,6 @@ import org.zeropage.causcheduler.R;
 
 public class PrefsFragment extends PreferenceFragment {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    // TODO: Rename and change types and number of parameters
     public static PrefsFragment newInstance() {
         PrefsFragment fragment = new PrefsFragment();
         return fragment;
@@ -29,16 +21,6 @@ public class PrefsFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.preferences);
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
     }
 
 }
