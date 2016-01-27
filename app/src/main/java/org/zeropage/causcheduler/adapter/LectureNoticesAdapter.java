51d45e69908ca676848f25a3861be1202d226000
Donghwan Kim <wraithtorch@gmail.com>
2016-01-27 09:14:44 +0900

    create base adapter

--- app/src/main/java/org/zeropage/causcheduler/adapter/LectureNoticesAdapter.java
+++ app/src/main/java/org/zeropage/causcheduler/adapter/LectureNoticesAdapter.java
@@ -6,18 +6,18 @@ import android.view.ViewGroup;
 import android.widget.ListAdapter;
 import io.realm.RealmBaseAdapter;
 import io.realm.RealmResults;
-import org.zeropage.causcheduler.data.Homework;
+import org.zeropage.causcheduler.data.LectureNotice;
 
 /**
  * Created by Donghwan on 2016-01-27.
  */
-public class HomeworksAdapter extends RealmBaseAdapter<Homework> implements ListAdapter{
+public class LectureNoticesAdapter extends RealmBaseAdapter<LectureNotice> implements ListAdapter{
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		return null;
 	}
 
-	public HomeworksAdapter(Context context, RealmResults<Homework> realmResults, boolean automaticUpdate) {
+	public LectureNoticesAdapter(Context context, RealmResults<LectureNotice> realmResults, boolean automaticUpdate) {
 		super(context, realmResults, automaticUpdate);
 	}
 }
--- app/src/main/java/org/zeropage/causcheduler/adapter/MealsAdapter.java
+++ app/src/main/java/org/zeropage/causcheduler/adapter/MealsAdapter.java
@@ -6,18 +6,18 @@ import android.view.ViewGroup;
 import android.widget.ListAdapter;
 import io.realm.RealmBaseAdapter;
 import io.realm.RealmResults;
-import org.zeropage.causcheduler.data.Homework;
+import org.zeropage.causcheduler.data.Meal;
 
 /**
  * Created by Donghwan on 2016-01-27.
  */
-public class HomeworksAdapter extends RealmBaseAdapter<Homework> implements ListAdapter{
+public class MealsAdapter extends RealmBaseAdapter<Meal> implements ListAdapter {
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		return null;
 	}
 
-	public HomeworksAdapter(Context context, RealmResults<Homework> realmResults, boolean automaticUpdate) {
+	public MealsAdapter(Context context, RealmResults<Meal> realmResults, boolean automaticUpdate) {
 		super(context, realmResults, automaticUpdate);
 	}
 }
