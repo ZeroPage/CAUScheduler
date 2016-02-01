b26719e44885813f24822ac3fc748abb8db6a688
Donghwan Kim <wraithtorch@gmail.com>
2016-02-01 12:48:46 +0900

    create adapters, but adapters can't update new data

--- app/src/main/java/org/zeropage/causcheduler/adapter/LectureNoticesAdapter.java
+++ app/src/main/java/org/zeropage/causcheduler/adapter/LectureNoticesAdapter.java
@@ -4,20 +4,44 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListAdapter;
+import android.widget.TextView;
 import io.realm.RealmBaseAdapter;
 import io.realm.RealmResults;
+import org.zeropage.causcheduler.R;
 import org.zeropage.causcheduler.data.LectureNotice;
+import org.zeropage.causcheduler.data.original.Lecture;
 
 /**
  * Created by Donghwan on 2016-01-27.
  */
 public class LectureNoticesAdapter extends RealmBaseAdapter<LectureNotice> implements ListAdapter{
+
+	private static class ViewHolder{
+		TextView item;
+	}
+
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		return null;
+		ViewHolder viewHolder;
+		if(convertView == null){
+			convertView = inflater.inflate(R.layout.list_item_lecture_notice, parent, false);
+			viewHolder = new ViewHolder();
+			viewHolder.item = (TextView)convertView.findViewById(R.id.lecture_notice_item_text);
+			convertView.setTag(viewHolder);
+		} else {
+			viewHolder = (ViewHolder) convertView.getTag();
+		}
+
+		// TODO 데이터 소스에서 해당 위치의 결과값 받아오기
+		// Object obj = realmResults.get(position);
+		// viewHolder.item.setText(obj.get());
+
+		return convertView;
 	}
 
 	public LectureNoticesAdapter(Context context, RealmResults<LectureNotice> realmResults, boolean automaticUpdate) {
 		super(context, realmResults, automaticUpdate);
 	}
+
+	public RealmResults<LectureNotice> getRealmResults() { return realmResults; }
 }
